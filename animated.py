374d59f3c9f89780aa9d94b827d5aa574ee9b780
khhandrea <khhandrea@kakao.com>
2021-03-05 00:25:54 +0900

    test edit

--- animated.py
+++ animated.py
@@ -16,7 +16,7 @@ def update_graph():
     plt.xticks(y_pos, objects)
     plt.ylabel('s (%)')
     fig.canvas.draw()
-    win.after(20000, update_graph)
+    win.after(20000, update_graph) # point!
 
 fig = plt.figure()
 win = fig.canvas.manager.window
